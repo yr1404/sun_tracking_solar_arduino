81d3f02e9fad9433d2fc8ac73d4d1b07689940af
Yash Raj <yash06765@gmail.com>
2023-04-21 01:05:46 +0530

    first commit

---
+++ code_suntrack.ino
@@ -0,0 +1,87 @@
+#include <Servo.h>
+
+Servo servo ;
+
+int eastLDR = 0;
+
+int westLDR = 1;
+
+int east = 0;
+
+int west = 0;
+
+int error = 0;
+
+int servoposition = 50;
+
+void setup()
+
+{
+
+  servo.attach(9);
+
+
+}
+
+void loop()
+
+{
+  east = analogRead(eastLDR);
+
+  west = analogRead(westLDR);
+
+  if (east < 350 && west < 350)
+
+  {
+
+    while (servoposition <= 150)
+
+    {
+
+      servoposition++;
+
+      servo.write(servoposition);
+
+      delay(100);
+
+    }
+
+  }
+
+  error = east - west;
+
+  if (error > 15)
+
+  {
+
+    if (servoposition <= 150)
+
+    {
+
+      servoposition++;
+
+      servo.write(servoposition);
+
+    }
+
+  }
+
+  else if (error < -15)
+
+  {
+
+    if (servoposition > 20)
+
+    {
+
+      servoposition--;
+
+      servo.write(servoposition);
+
+    }
+
+  }
+
+  delay(50);
+
+}
